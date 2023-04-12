e125a07d4848f0d75f146b2999f2b73e4f204ca6
sanjayamal <rajithasanjayamal0918@gmail.com>
2023-04-12 06:56:36 +0530

    persnoal

--- src/components/About/PersonalInfo/Components/InfoItem/InfoItem.tsx
+++ src/components/About/PersonalInfo/Components/InfoItem/InfoItem.tsx
@@ -1,4 +1,4 @@
-import { Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import React from "react";
 
 interface IInfoItem {
@@ -7,12 +7,12 @@ interface IInfoItem {
 }
 const InfoItem: React.FC<IInfoItem> = ({ value, label }) => {
   return (
-    <Typography sx={{ color: "rgba(0, 0, 0, 0.54)" }}>
-      {label} :{" "}
-      <span style={{ color: "rgba(0, 0, 0, 0.54)", fontWeight: "450" }}>
+    <Box>
+      <Typography sx={{ color: "rgba(0, 0, 0, 0.54)" }}>{label}</Typography>
+      <Typography sx={{ color: "rgba(0, 0, 0, 0.54)", fontWeight: "450" }}>
         {value}
-      </span>
-    </Typography>
+      </Typography>
+    </Box>
   );
 };
 
